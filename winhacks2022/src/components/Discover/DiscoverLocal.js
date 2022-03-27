e2a13a7b9514c66f042617223afa3c1387207d09
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-03-27 00:14:18 -0700

    Quick update

--- winhacks2022/src/components/Discover/DiscoverLocal.js
+++ winhacks2022/src/components/Discover/DiscoverLocal.js
@@ -22,16 +22,129 @@ export class DiscoverLocal extends Component {
                   <div className='recommendedTitle'>
                       <p>Recommended</p> <button>see all <img src='./images/rightarrow.png' /></button>
                   </div>
+                  <div className='restrauntList'>
+                  <div className='restraunt1'>
+                          <div className='restrauntHeader1'>
+                              <img className='restrauntCover' src='./images/dominoscover.png' />
+                          </div>
+                          <div className='restrauntInfo1'>
+                              <div className='restrauntInfoTop'>
+                                  <p>Domino's Pizza</p>
+                                  <a>10:00 AM - 5:00 PM</a>
+                              </div>
+                              <div className='restrauntInfoBottom'>
+                                  <div className='fix'>
+                                  <img src='./images/rating.png' /> <p>3.9 | 10 Miles</p>
+                                  </div>
+                                  <div className='fix'>
+                                  <p className='dollar'>$4.99</p>
+                                  </div>
+                              </div>
+                          </div>
+                    </div>
+                    <div className='restraunt1'>
+                          <div className='restrauntHeader1'>
+                              <img className='restrauntCover' src='./images/starbucks.png' />
+                          </div>
+                          <div className='restrauntInfo1'>
+                              <div className='restrauntInfoTop'>
+                                  <p>Starbucks</p>
+                                  <a>10:00 AM - 5:00 PM</a>
+                              </div>
+                              <div className='restrauntInfoBottom'>
+                                  <div className='fix'>
+                                  <img src='./images/rating.png' /> <p>4.9 | 12 Miles</p>
+                                  </div>
+                                  <div className='fix'>
+                                  <p className='dollar'>$4.99</p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className='restraunt1'>
+                          <div className='restrauntHeader1'>
+                              <img className='restrauntCover' src='./images/saveonfoods.png' />
+                          </div>
+                          <div className='restrauntInfo1'>
+                              <div className='restrauntInfoTop'>
+                                  <p>Grocery Store</p>
+                                  <a>10:00 AM - 5:00 PM</a>
+                              </div>
+                              <div className='restrauntInfoBottom'>
+                                  <div className='fix'>
+                                  <img src='./images/rating.png' /> <p>3.7 | 13 Miles</p>
+                                  </div>
+                                  <div className='fix'>
+                                  <p className='dollar'>$4.99</p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      </div>
               </div>
               <div className='recommended'>
                   <div className='recommendedTitle'>
-                      <p>Recommended</p> <button>see all <img src='./images/rightarrow.png' /></button>
+                      <p>My Favourite's</p> <button>see all <img src='./images/rightarrow.png' /></button>
                   </div>
-              </div>
-              <div className='recommended'>
-                  <div className='recommendedTitle'>
-                      <p>Recommended</p> <button>see all <img src='./images/rightarrow.png' /></button>
-                  </div>
+                  <div className='restrauntList'>
+                  <div className='restraunt1'>
+                          <div className='restrauntHeader1'>
+                              <img className='restrauntCover' src='./images/Hugos.png' />
+                          </div>
+                          <div className='restrauntInfo1'>
+                              <div className='restrauntInfoTop'>
+                                  <p>Hugo's Tacos</p>
+                                  <a>10:00 AM - 5:00 PM</a>
+                              </div>
+                              <div className='restrauntInfoBottom'>
+                                  <div className='fix'>
+                                  <img src='./images/rating.png' /> <p>3.9 | 10 Miles</p>
+                                  </div>
+                                  <div className='fix'>
+                                  <p className='dollar'>$4.99</p>
+                                  </div>
+                              </div>
+                          </div>
+                    </div>
+                    <div className='restraunt1'>
+                          <div className='restrauntHeader1'>
+                              <img className='restrauntCover' src='./images/Maki.png' />
+                          </div>
+                          <div className='restrauntInfo1'>
+                              <div className='restrauntInfoTop'>
+                                  <p>Maki's Sushi</p>
+                                  <a>10:00 AM - 5:00 PM</a>
+                              </div>
+                              <div className='restrauntInfoBottom'>
+                                  <div className='fix'>
+                                  <img src='./images/rating.png' /> <p>4.4 | 8 Miles</p>
+                                  </div>
+                                  <div className='fix'>
+                                  <p className='dollar'>$4.99</p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      <div className='restraunt1'>
+                          <div className='restrauntHeader1'>
+                              <img className='restrauntCover' src='./images/dominoscover.png' />
+                          </div>
+                          <div className='restrauntInfo1'>
+                              <div className='restrauntInfoTop'>
+                                  <p>Domino's Pizza</p>
+                                  <a>10:00 AM - 5:00 PM</a>
+                              </div>
+                              <div className='restrauntInfoBottom'>
+                                  <div className='fix'>
+                                  <img src='./images/rating.png' /> <p>3.9 | 10 Miles</p>
+                                  </div>
+                                  <div className='fix'>
+                                  <p className='dollar'>$4.99</p>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                      </div>
               </div>
           </div>
       )
