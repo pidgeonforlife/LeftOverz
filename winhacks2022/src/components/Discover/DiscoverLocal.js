1b2cd23b847f916abfeca070abad85ff54ddd3b5
Eric Choi <77644021+pidgeonforlife@users.noreply.github.com>
2022-03-26 22:16:10 -0700

    Pushing update

--- winhacks2022/src/components/Discover/DiscoverLocal.js
+++ winhacks2022/src/components/Discover/DiscoverLocal.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-
+import HorizontalScroll from 'react-scroll-horizontal'
+import './Discover.css';
 
 export class DiscoverLocal extends Component {
     render() {
       return (
           <div className='signUpContainer'>
-              <div className='signUpTitle'>
+              <div className='discoverTitle'>
                   <p className='move'>DISCOVER</p>
+                  <button className='doThis'> <img src='./images/loco.png' /> Vancouver, BC</button>
               </div>
-              <div className='SignUpForm'>
-                  <form>
-                      <label>Enter your city</label>
-                      <input
-                        type='text'
-                        placeholder='Location' />
-                  </form>
+              <div className='DiscoverFoods'>
+                  <button><img src='./images/hamburger.png' /> Fast Food</button>
+                  <button><img src='./images/burrito.png' /> Mexican</button>
+                  <button><img src='./images/pizza.png' /> Pizza</button>
+                  <button><img src='./images/sushi.png' /> Sushi</button>
+                  <button><img src='./images/salad.png' /> Vegan</button>
+                  <button><img src='./images/drinks.png' /> Drinks</button>
               </div>
-              <div className='locationHolder'>
-                  <img src='./images/orangearrow.png'/>
-                  <p>Use current location</p>
+              <div className='recommended'>
+                  <div className='recommendedTitle'>
+                      <p>Recommended</p> <button>see all <img src='./images/rightarrow.png' /></button>
+                  </div>
+              </div>
+              <div className='recommended'>
+                  <div className='recommendedTitle'>
+                      <p>Recommended</p> <button>see all <img src='./images/rightarrow.png' /></button>
+                  </div>
+              </div>
+              <div className='recommended'>
+                  <div className='recommendedTitle'>
+                      <p>Recommended</p> <button>see all <img src='./images/rightarrow.png' /></button>
+                  </div>
               </div>
           </div>
       )
